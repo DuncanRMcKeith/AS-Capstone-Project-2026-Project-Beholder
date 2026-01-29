ba8b9f77a832f566bb5d44dafc431c6630317954
Rblye22 <rpblye@email.neit.edu>
2026-01-29 13:03:43 -0500

    update from class
    worked on announcements

--- CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
+++ CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53e1a8c1840cb8331ed295e4885c3afe40304c4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ba257acfc7f4678a148adad33d8abd8c46d778")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
