d9427841b25210c97d6daa480d581b7de265cb76
DuncanRMcKeith <duncanmckeith3@gmail.com>
2026-02-04 12:37:34 -0500

    adding in robs pages again

--- CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
+++ CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00741cfe42b97d31df0f40fadeb0a095e992249d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f35626d64adc93f23fdecb2261e3c97297d75aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
