8c35396f4040d2880acd1741f54dd4fab18da186
DuncanRMcKeith <duncanmckeith3@gmail.com>
2026-02-04 11:59:37 -0500

    Andrews Join

--- CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
+++ CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c6021a9946640351db41b8b1345a915f2527817")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b69989361724e7d5e9a637c70ef0a9175b55ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
