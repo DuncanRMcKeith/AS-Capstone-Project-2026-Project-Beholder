d91381918818bf1b12032576391f7c05b70e12ed
DuncanRMcKeith <duncanmckeith3@gmail.com>
2026-02-04 12:51:48 -0500

    Got it working

--- CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
+++ CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45717b70ac64ad75d30e34c7437a8826baa622e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8aeacf9671180be107ee37837aeeeb3df439763")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
