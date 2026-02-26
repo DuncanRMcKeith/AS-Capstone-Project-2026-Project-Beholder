a895c8193df6721cca59669b2476ade94ff54013
KyleGuerriero <kjgguerriero@gmail.com>
2026-02-25 20:32:30 -0500

    Removed Chat Code, Created Create Community
    Removed the chat code as another member was able to get the back end functionality. Fixed up the community page and added a button that will allow a logged in user create a community.

--- CapstoneProject/Pages/Community.cshtml.cs
+++ CapstoneProject/Pages/Community.cshtml.cs
@@ -7,22 +7,53 @@ namespace CapstoneProject.Pages
     public class CommunityModel : PageModel
     {
         private readonly IConfiguration _configuration;
-
         CommunityDataAccessLayer factory;
 
-        // Change the type to CapstoneProject.Models.CommunityModel
         public List<CapstoneProject.Models.CommunityModel> Communities { get; set; }
+        public List<int> JoinedCommunityIds { get; set; } = new List<int>();
 
         public CommunityModel(IConfiguration configuration)
         {
             _configuration = configuration;
-
             factory = new CommunityDataAccessLayer(configuration);
         }
 
         public void OnGet()
         {
             Communities = factory.GetAll().ToList();
+
+            // Get the communities the user has already joined
+            var userIdFromSession = HttpContext.Session.GetInt32("UserID");
+            if (userIdFromSession.HasValue)
+            {
+                JoinedCommunityIds = factory.GetUserCommunities(userIdFromSession.Value)
+                    .Select(c => c.CommunityID)
+                    .ToList();
+            }
+        }
+
+        public IActionResult OnPostJoin(int communityId)
+        {
+            var userIdFromSession = HttpContext.Session.GetInt32("UserID");
+            if (!userIdFromSession.HasValue)
+            {
+                return RedirectToPage("/Login");
+            }
+
+            factory.JoinCommunity(userIdFromSession.Value, communityId);
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostLeave(int communityId)
+        {
+            var userIdFromSession = HttpContext.Session.GetInt32("UserID");
+            if (!userIdFromSession.HasValue)
+            {
+                return RedirectToPage("/Login");
+            }
+
+            factory.LeaveCommunity(userIdFromSession.Value, communityId);
+            return RedirectToPage();
         }
     }
 }
